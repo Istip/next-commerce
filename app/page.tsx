61fe84dd30301756e0884e1d2ac0f8c1845825e7
Pasztor Isti <isticsek@gmail.com>
2023-11-18 13:56:26 +0200

    add hero to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-medium">Hello!</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
-        eligendi, est vero non nam modi placeat assumenda, consequatur sapiente
-        recusandae quis ullam repellat tempore ex tempora dicta cumque nulla
-        asperiores.
-      </p>
+    <div className="bg-stone-50 pb-6 sm:pb-8 lg:pb-12">
+      <Hero />
     </div>
   );
 }
