cba9fe0a4f8d392255009109c029108f8baab9ce
Pasztor Isti <isticsek@gmail.com>
2023-11-18 13:49:17 +0200

    create and implement navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { name: "Home", href: "/" },
+  { name: "Men", href: "/Men" },
+  { name: "Women", href: "/Women" },
+  { name: "Teens", href: "/Teens" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <header className="mb-8 border-b">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <Link href="/">
+          <h1 className="font-black text-2xl">
+            Next<span className="text-primary">Commerce</span>
+          </h1>
+        </Link>
+
+        <nav>
+          <ul className="hidden gap-12 lg:flex 2xl:ml-16">
+            {links.map((link, id) => (
+              <li key={id}>
+                {pathname === link.href ? (
+                  <Link
+                    className="text-lg font-semibold text-primary"
+                    href={link.href}
+                  >
+                    {link.name}
+                  </Link>
+                ) : (
+                  <Link
+                    href={link.href}
+                    className="text-lg font-semibold text-stone-600 transition duration-200 hover:text-primary"
+                  >
+                    {link.name}
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="flex divide-x border-r sm:border-l">
+          <Button
+            variant="outline"
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+          >
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold text-stone-500 sm:block">
+              CART
+            </span>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
